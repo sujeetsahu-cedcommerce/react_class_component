2d9565550d30d8ed282857e3de5fcde6d23b4a9e
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-06-30 18:46:21 +0530

    Q3. Make the following component structure using class components.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@
 import './App.css';
 // import Components from './Components';
 // import ArrowFunction from './ArrowFunction';
-import OperatingSystem from './OperatingSystem';
-import Manufaturers from './Manufaturers';
+// import OperatingSystem from './OperatingSystem';
+// import Manufaturers from './Manufaturers';
+import {Console} from './Structure';
+import { Playground } from './Structure';
+import { Adventure } from './Structure';
 function App() {
   return (
-    <div >
-      
+    <div className="parent">
      {/* <Components></Components> */}
      {/* <ArrowFunction></ArrowFunction> */}
-     <OperatingSystem></OperatingSystem>
-     <Manufaturers></Manufaturers>
+     {/* <OperatingSystem></OperatingSystem>
+     <Manufaturers></Manufaturers> */}
+     <Console/>
+     <Playground/>
+     <Adventure/>
     </div>
   );
 }
---
+++ src/Structure.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import "./Mystyle.css";
+
+export class Console extends Component {
+  render() {
+    return (
+      <div className="text">
+        <div className="heading">Adventure</div>
+        <div className="para">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididun et dolore magna aliqua. Ut enim reprehenderit
+          in vo cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+          id est laborum.
+        </div>
+        <button className="Go_Anywhere">Go Anywhere</button>
+      </div>
+    );
+  }
+}
+
+export class Playground extends Component {
+  render() {
+    return (
+      <div className="text">
+      <div className="heading">Adventure</div>
+      <div className="para">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididun et dolore magna aliqua. Ut enim reprehenderit
+        in vo cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+        id est laborum.
+      </div>
+      <button className="Go_Anywhere">Go Anywhere</button>
+    </div>
+    );
+  }
+}
+
+export class Adventure extends Component {
+  render() {
+    return (
+      <div className="text">
+        <div className="heading">Adventure</div>
+        <div className="para">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididun et dolore magna aliqua. Ut enim reprehenderit
+          in vo cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+          id est laborum.
+        </div>
+        <button className="Go_Anywhere">Go Anywhere</button>
+      </div>
+    );
+  }
+}
