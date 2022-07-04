2b140c9b396d88de880f028a2bfdc43c9edf569a
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-07-04 21:10:34 +0530

    react task

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
-// import Components from './Components';
-// import ArrowFunction from './ArrowFunction';
-// import OperatingSystem from './OperatingSystem';
-// import Manufaturers from './Manufaturers';
+import Components from './Components';
+import ArrowFunction from './ArrowFunction';
+import OperatingSystem from './OperatingSystem';
+import Manufaturers from './Manufaturers';
 import {Console} from './Structure';
 import { Playground } from './Structure';
 import { Adventure } from './Structure';
 function App() {
   return (
     <div className="parent">
-     {/* <Components></Components> */}
-     {/* <ArrowFunction></ArrowFunction> */}
-     {/* <OperatingSystem></OperatingSystem>
-     <Manufaturers></Manufaturers> */}
+     <Components></Components> 
+     <ArrowFunction></ArrowFunction> 
+     <OperatingSystem></OperatingSystem>
+     <Manufaturers></Manufaturers>
      <Console/>
      <Playground/>
      <Adventure/>
